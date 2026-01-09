90113b4acbc41c8d7b2b2f09ea72a5db89bacbfb
Ipsita Dutta <157873351+ipsita5-dut@users.noreply.github.com>
2026-01-09 09:12:36 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,19 @@
+package corejava;
+
+public class First {
+	public static void main(String[] args) {
+		String name="Dinga";
+		String PhoneNo= "9007366772";
+		String gender="Male";
+		boolean married=false;
+		int age=21;
+		double weight=69.5;
+		
+		System.out.println(name);
+		System.out.println(PhoneNo);
+		System.out.println(gender);
+		System.out.println(married);
+		System.out.println(age);
+		System.out.println(weight);
+	}	
+}
---
+++ Second.java
@@ -0,0 +1,34 @@
+package corejava;
+
+public class Second {
+	public static void main(String[] args) {
+		int a=2;
+		int b=4;
+		float c=3.2f;
+		String s="abc";
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.println(a+c);
+		System.out.println(a-c);
+		System.out.println(a*c);
+		System.out.println(a/c);
+		System.out.println(a%c);
+		System.out.println(a+s);
+		System.out.println(s+a);
+		System.out.println(s+c);
+		System.out.println(s+a+s);
+		
+		int p= 2, q=6;
+		System.out.println(p==q);
+		System.out.println(p>=q);
+		System.out.println(p<=q);
+		System.out.println(p>q);
+		System.out.println(p<q);
+
+		
+	}
+	
+}
